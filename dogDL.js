71c7681d13ee129d9cebef95a3e06a67cfb787d2
MrBoogyBam <MrBoogyBamBusiness@gmail.com>
2020-07-20 00:23:52 +0300

    dog website

---
+++ dogDL.js
@@ -0,0 +1,12 @@
+function changeImage(i) {
+    if(i == 1 )
+    document.getElementsByTagName('img')[0].src = 'Dog1.jpg';
+    else if(i == 2 )
+    document.getElementsByTagName('img')[0].src = 'Dog2.jpg';
+    else if(i == 3 )
+    document.getElementsByTagName('img')[0].src = 'Dog3.jpg';
+    else if(i == 4 )
+    document.getElementsByTagName('img')[0].src = 'Dog4.jpg';
+    else if(i == 5 )
+    document.getElementsByTagName('img')[0].src = 'Dog5.jpg';
+}
